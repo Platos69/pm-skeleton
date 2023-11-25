3690927e213635d9d4e399514e4e01ea3a8f789b
Daniel <daniefelipe2905@gmail.com>
2023-11-25 18:57:22 -0300

    adicionado

--- routes/admin.js
+++ routes/admin.js
@@ -138,68 +138,70 @@ router.get('/postagens', ((req, res) => {
         res.redirect('/admin')
     })
 }))
-
-router.get('/postagens/add', ((req, res) => {
-    Categoria.find().then((categorias) => {
-        res.render('admin/addpostagens', { categorias: categorias })
-    }).catch((erro) => {
-        req.flash('error_msg', 'Houve um error ao carregar o formulário')
-        res.render('admin/postagens')
-    })
-}))
-
-router.post('/postagens/nova', ((req, res) => {
-    var erros = []
-
-    if (!req.body.titulo || typeof req.body.titulo == undefined || typeof req.body.titulo == null) {
-        erros.push({ texto: "Nome inválido" })
-    } else if (req.body.titulo.length < 2) {
-        erros.push({ texto: "Título da postagem muito pequeno" })
-    }
-
-    if (!req.body.slug || typeof req.body.slug == undefined || typeof req.body.slug == null) {
-        erros.push({ texto: "Slug inválido" })
-    } else if (req.body.slug.length < 2) {
-        erros.push({ texto: "Slug muito pequeno" })
-    }
-
-    if (!req.body.conteudo || typeof req.body.conteudo == undefined || typeof req.body.conteudo == null) {
-        erros.push({ texto: "Conteúdo inválido" })
-    } else if (req.body.slug.length < 2) {
-        erros.push({ texto: "Conteúdo muito pequeno" })
-    }
-
-    if (!req.body.descricao || typeof req.body.descricao == undefined || typeof req.body.descricao == null) {
-        erros.push({ texto: "Descrição inválida" })
-    } else if (req.body.descricao.length < 2) {
-        erros.push({ texto: "Descrição muito pequena" })
-    }
-
-    if (req.body.categoria == '0') {
-        erros.push({ texto: 'Não há categorias criadas' })
-    }
-
-    if (erros.length > 0) {
-        res.render('admin/addpostagens', { erros: erros })
-    } else {
-
-        const novaPostagem = {
-            titulo: req.body.titulo,
-            slug: req.body.slug,
-            descricao: req.body.descricao,
-            conteudo: req.body.conteudo,
-            categoria: req.body.categoria
-        }
-
-        new Postagem(novaPostagem).save().then(() => {
-            req.flash('success_msg', 'Postagem criada com sucesso')
-            res.redirect('/admin/postagens')
+    // Página de adição
+    router.get('/postagens/add', ((req, res) => {
+        Categoria.find().then((categorias) => {
+            res.render('admin/addpostagens', { categorias: categorias })
         }).catch((erro) => {
-            req.flash('error_msg', 'Falha na criação da postagem')
-            res.redirect('/admin/postagens')
+            req.flash('error_msg', 'Houve um error ao carregar o formulário')
+            res.render('admin/postagens')
         })
-    }
-}))
+    }))
+        // Direcionamento para validação
+        router.post('/postagens/nova', ((req, res) => {
+            var erros = []
+
+            // Validações
+            if (!req.body.titulo || typeof req.body.titulo == undefined || typeof req.body.titulo == null) {
+                erros.push({ texto: "Nome inválido" })
+            } else if (req.body.titulo.length < 2) {
+                erros.push({ texto: "Título da postagem muito pequeno" })
+            }
+
+            if (!req.body.slug || typeof req.body.slug == undefined || typeof req.body.slug == null) {
+                erros.push({ texto: "Slug inválido" })
+            } else if (req.body.slug.length < 2) {
+                erros.push({ texto: "Slug muito pequeno" })
+            }
+
+            if (!req.body.conteudo || typeof req.body.conteudo == undefined || typeof req.body.conteudo == null) {
+                erros.push({ texto: "Conteúdo inválido" })
+            } else if (req.body.slug.length < 2) {
+                erros.push({ texto: "Conteúdo muito pequeno" })
+            }
+
+            if (!req.body.descricao || typeof req.body.descricao == undefined || typeof req.body.descricao == null) {
+                erros.push({ texto: "Descrição inválida" })
+            } else if (req.body.descricao.length < 2) {
+                erros.push({ texto: "Descrição muito pequena" })
+            }
+
+            if (req.body.categoria == '0') {
+                erros.push({ texto: 'Não há categorias criadas' })
+            }
+                // Mostra erros
+                if (erros.length > 0) {
+                    res.render('admin/addpostagens', { erros: erros })
+                } else {
+
+                    // Adiciona ao banco de dados
+                    const novaPostagem = {
+                        titulo: req.body.titulo,
+                        slug: req.body.slug,
+                        descricao: req.body.descricao,
+                        conteudo: req.body.conteudo,
+                        categoria: req.body.categoria
+                    }
+
+                    new Postagem(novaPostagem).save().then(() => {
+                        req.flash('success_msg', 'Postagem criada com sucesso')
+                        res.redirect('/admin/postagens')
+                    }).catch((erro) => {
+                        req.flash('error_msg', 'Falha na criação da postagem')
+                        res.redirect('/admin/postagens')
+                    })
+            }
+    }))
 
 
 module.exports = router
